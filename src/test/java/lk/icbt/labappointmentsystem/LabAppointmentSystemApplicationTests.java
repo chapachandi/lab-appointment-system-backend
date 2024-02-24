df429ae44dc53327c1be20fcd41db73e60822f77
Chapa <chapachandi23@gmail.com>
2024-02-24 23:47:35 +0530

    create project

---
+++ src/test/java/lk/icbt/labappointmentsystem/LabAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.icbt.labappointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
