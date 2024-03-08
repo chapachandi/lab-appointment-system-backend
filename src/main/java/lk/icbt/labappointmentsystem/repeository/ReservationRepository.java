6d3108bca9a52338e9ed518af130d74be86f4be0
Chapa <chapachandi23@gmail.com>
2024-03-08 14:42:14 +0530

    change code using jwt

--- src/main/java/lk/icbt/labappointmentsystem/repeository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.icbt.labappointmentsystem.repeository;
-
-import lk.icbt.labappointmentsystem.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
--- src/main/java/lk/icbt/labappointmentsystem/repeository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.icbt.labappointmentsystem.repeository;
-
-import lk.icbt.labappointmentsystem.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Integer> {
-}
